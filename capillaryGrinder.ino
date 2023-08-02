f6751e98fe35d4ef863d8831c308aa9a80ee5b27
fwappy <Ezra.kainz@gmail.com>
2023-08-01 17:05:37 -0700

    Write pseudocode

--- capillaryGrinder.ino
+++ capillaryGrinder.ino
@@ -1,6 +1,7 @@
 #include "capillaryGrinder_menu.h"
 
 void setup() {
+    Serial.begin(9600);
     setupMenu();
 
 }
@@ -12,12 +13,33 @@ void loop() {
 
 
 
+void CALLBACK_FUNCTION setZeroed(int id) {
+    // if selection is "ignore" or (id = NULL)
+    //  Make start button visible/hide not zeroed selector for all categories
+    // if selection is "zero" call CalibrateZero
+}
+
+void CALLBACK_FUNCTION CalibrateZero(int id) {
+    // Software travels down at 10 micron per second until sound signal digital output of “1” is received. 
+    // Moves up 10 microns
+    // Lowers at 1 micron a second until sound signal “1” is sent. (maximum 15 micron travel)
+    // reset currentOffset
+    // call moveOffset()
+    // Call setZeroed()
+}
+
+
+void CALLBACK_FUNCTION moveOffset(int id) {
+    // move to match currentOffset to offset
+}
+
+
 void CALLBACK_FUNCTION startGrindCapillary(int id) {
     // TODO - your menu change code
 }
 
 
-void CALLBACK_FUNCTION setZeroed(int id) {
+void CALLBACK_FUNCTION startFaceCapillary(int id) {
     // TODO - your menu change code
 }
 
@@ -28,20 +50,5 @@ void CALLBACK_FUNCTION startFaceChipParametric(int id) {
 
 
 void CALLBACK_FUNCTION startFaceChipDistance(int id) {
-    // TODO - your menu change code
-}
-
-
-void CALLBACK_FUNCTION CalibrateZero(int id) {
-    // TODO - your menu change code
-}
-
-
-void CALLBACK_FUNCTION moveOffset(int id) {
-    // TODO - your menu change code
-}
-
-
-void CALLBACK_FUNCTION startFaceCapillary(int id) {
     // TODO - your menu change code
 }
